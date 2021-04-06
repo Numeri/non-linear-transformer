112fc22388050266af7fa00bcd3e4d64940ad5e0
Numeri <Numeri@users.noreply.github.com>
2021-04-05 23:39:18 -0600

    Added model hyperparameters

--- hyperparameters.py
+++ hyperparameters.py
@@ -6,8 +6,13 @@ class Hyperparameters:
     batch_size : int
     learning_rate : float
     epochs : int
+    deterministic : bool
+    training_attn_dropout : float
 
     # Model parameters
     d_model : int
     seq_length : int
+    vocabulary_size : int
     num_heads : int
+    num_encoders : int
+    num_decoders : int
