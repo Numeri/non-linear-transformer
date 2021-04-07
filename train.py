c5a6a3896cdd59aa900b57bf6a3eeb8f3297a18e
Numeri <Numeri@users.noreply.github.com>
2021-04-06 21:59:26 -0600

    Removed hypers, moved to hyperparameters.py

--- train.py
+++ train.py
@@ -4,24 +4,11 @@ import jax
 from flax import linen as nn
 from jax import numpy as np, random
 
-from hyperparameters import Hyperparameters
+from hyperparameters import Hyperparameters, hypers
 from model import Transformer
 
 
 np.set_printoptions(edgeitems=30, linewidth=190)
-hypers = Hyperparameters(batch_size            = 10,
-                         learning_rate         = 0.01,
-                         epochs                = 1000,
-                         deterministic         = False,
-                         training_attn_dropout = 0.05,
-
-                         d_model               = 512 // 2,
-                         seq_length            = 256 // 2,
-                         vocabulary_size       = 1000 // 2,
-                         num_heads             = 8,
-                         num_encoders          = 6,
-                         num_decoders          = 6,
-                         )
 
 key = random.PRNGKey(10)
 model = Transformer(hypers)
