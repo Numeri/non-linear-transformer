f3c0c93384cfd30d3dfebec4ef33e3f700c2a67f
Numeri <Numeri@users.noreply.github.com>
2021-04-06 21:58:55 -0600

    Moved hypers into its own file

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from dataclasses import dataclass
 
 @dataclass
@@ -13,6 +14,32 @@ class Hyperparameters:
     d_model : int
     seq_length : int
     vocabulary_size : int
+    extra_tokens : Tuple[str]
     num_heads : int
     num_encoders : int
     num_decoders : int
+
+    # Dataset parameters
+    dataset : str
+    model_folder : str
+    vocabulary_prefix : str
+
+hypers = Hyperparameters(batch_size            = 10,
+                         learning_rate         = 0.01,
+                         epochs                = 1000,
+                         deterministic         = False,
+                         training_attn_dropout = 0.05,
+
+                         d_model               = 512 // 2,
+                         seq_length            = 256 // 2,
+                         vocabulary_size       = 1000 // 2,
+                         extra_tokens          = ('<empty>',),
+                         num_heads             = 8,
+                         num_encoders          = 6,
+                         num_decoders          = 6,
+
+                         dataset               = 'wmt14_translate/de-en',
+                         model_folder          = 'models',
+                         vocabulary_prefix     = 'sentencepiece',
+                         )
+
